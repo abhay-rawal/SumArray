54aa452803e9efff1545b5f3a466eb2b985426b0
abhayrwl1996@outlook.com <abhayrwl1996@outlook.com>
2020-10-14 14:21:19 +0545

    Add project files.

---
+++ SumArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArray
+{
+    class Program
+    {
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int counter = 0;
+            int lengthOfNums = nums.Length;
+            int i, j;
+            int[] sumArray = new int[nums.Length];
+            sumArray[0] = nums[0];
+
+            for (i = 1; i < lengthOfNums; i++)
+            {
+                int totalCount = 0;
+                for (j = 0; j < i + 1; j++)
+                {
+                    totalCount = totalCount + nums[j];
+                }
+                sumArray[i] = totalCount;
+
+            }
+            return sumArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            var result = RunningSum(nums);
+           
+        }
+    }
+}
